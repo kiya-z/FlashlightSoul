5d73d354a39c4abed9d2001ae24cac17842bddf9
kiya-z <2267161320@qq.com>
2016-02-04 19:04:47 +0800

    open&close flashlight

--- app/src/main/java/space/kiya/flashlightsoul/BaseActivity.java
+++ app/src/main/java/space/kiya/flashlightsoul/BaseActivity.java
@@ -1,22 +1,24 @@
 package space.kiya.flashlightsoul;
 import android.app.Activity;
 import android.os.Bundle;
+import android.hardware.Camera;
 import android.widget.ImageView;
 /**
  * Created by kiya on 16-2-3.
  */
 public class BaseActivity extends Activity {
 
-    protected ImageView mFlashlight;
-    protected ImageView mFlashlightController;
+    protected ImageView mImageViewFlashlight;
+    protected ImageView mImageViewFlashlightController;
+    protected Camera mCamera;
+    protected Camera.Parameters mParameters;
 
-    @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            mFlashlight = (ImageView) findViewById(R.id.flashlight);
-            mFlashlightController = (ImageView) findViewById(R.id.flashlight_controller);
-        }
+        mImageViewFlashlight = (ImageView) findViewById(R.id.flashlight);
+        mImageViewFlashlightController = (ImageView) findViewById(R.id.flashlight_controller);
+    }
 }
 
--- app/src/main/java/space/kiya/flashlightsoul/Flashlight.java
+++ app/src/main/java/space/kiya/flashlightsoul/Flashlight.java
@@ -1,11 +1,16 @@
 package space.kiya.flashlightsoul;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
+import android.graphics.SurfaceTexture;
+import android.graphics.drawable.TransitionDrawable;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Toast;
+
+import java.io.IOException;
 /**
  * Created by kiya on 16-2-3.
  */
@@ -14,14 +19,23 @@ public class Flashlight extends BaseActivity{
     @Override public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
 
+        mImageViewFlashlight.setTag(false);
+
         Point point = new Point();
         getWindowManager().getDefaultDisplay().getSize(point);
 
-        LayoutParams layoutParams = mFlashlightController.getLayoutParams();
+        LayoutParams layoutParams = mImageViewFlashlightController.getLayoutParams();
         layoutParams.height = point.y * 3/4;
         layoutParams.width = point.x / 3;
 
-        mFlashlightController.setLayoutParams(layoutParams);
+        mImageViewFlashlightController.setLayoutParams(layoutParams);
+    }
+
+    @Override protected void onPause() {
+        super.onPause();
+        if ((Boolean) mImageViewFlashlight.getTag()) {
+            closeFlashlight();
+        }
     }
 
     public void onclick_flashlight(View view){
@@ -29,6 +43,73 @@ public class Flashlight extends BaseActivity{
             Toast.makeText(this,"您的设备不支持闪光灯",Toast.LENGTH_SHORT).show();
             return;
         }
-        Toast.makeText(this,"您的设备支持闪光灯!",Toast.LENGTH_SHORT).show();
+        if (mImageViewFlashlight == null){
+            Toast.makeText(this,"kong!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(this,mImageViewFlashlight.getTag().toString(),Toast.LENGTH_SHORT).show();
+        if (!(Boolean) mImageViewFlashlight.getTag()){
+            openFLashlight();
+        }else {
+            closeFlashlight();
+        }
+    }
+
+
+    protected void openFLashlight(){
+        TransitionDrawable transitionDrawable = (TransitionDrawable) mImageViewFlashlight.getDrawable();
+        transitionDrawable.startTransition(200);
+        mImageViewFlashlight.setTag(true);
+
+        try {
+            mCamera = Camera.open();
+            int textureId = 0;
+            mCamera.setPreviewTexture(new SurfaceTexture(textureId));
+            mCamera.startPreview();
+
+            mParameters = mCamera.getParameters();
+            mParameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            mCamera.setParameters(mParameters);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void closeFlashlight(){
+        TransitionDrawable transitionDrawable = (TransitionDrawable) mImageViewFlashlight.getDrawable();
+        if (!(Boolean) mImageViewFlashlight.getTag()){
+            transitionDrawable.startTransition(200);
+            mImageViewFlashlight.setTag(false);
+
+            if (mCamera != null){
+                mParameters = mCamera.getParameters();
+                mParameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                mCamera.setParameters(mParameters);
+                mCamera.stopPreview();
+                mCamera.release();
+                mCamera = null;
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
