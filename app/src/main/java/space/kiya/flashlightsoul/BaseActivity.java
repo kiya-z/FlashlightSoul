2212d4713ef2d37e58e42e9545fed636459a07f0
kiya-z <2267161320@qq.com>
2016-02-04 15:08:59 +0800

    add controller area

--- app/src/main/java/space/kiya/flashlightsoul/BaseActivity.java
+++ app/src/main/java/space/kiya/flashlightsoul/BaseActivity.java
@@ -1,15 +1,22 @@
 package space.kiya.flashlightsoul;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 /**
  * Created by kiya on 16-2-3.
  */
 public class BaseActivity extends Activity {
 
+    protected ImageView mFlashlight;
+    protected ImageView mFlashlightController;
+
     @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+
+            mFlashlight = (ImageView) findViewById(R.id.flashlight);
+            mFlashlightController = (ImageView) findViewById(R.id.flashlight_controller);
         }
 }
 
--- app/src/main/java/space/kiya/flashlightsoul/Flashlight.java
+++ app/src/main/java/space/kiya/flashlightsoul/Flashlight.java
@@ -1,7 +1,23 @@
 package space.kiya.flashlightsoul;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.ViewGroup.LayoutParams;
 /**
  * Created by kiya on 16-2-3.
  */
 public class Flashlight extends BaseActivity{
 
+    @Override public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+
+        LayoutParams layoutParams = mFlashlightController.getLayoutParams();
+        layoutParams.height = point.y * 3/4;
+        layoutParams.width = point.x / 3;
+
+        mFlashlightController.setLayoutParams(layoutParams);
+    }
 }
