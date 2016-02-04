ce84f0f7c98ce3242c7ddf3ff11167a2687f53e9
kiya-z <2267161320@qq.com>
2016-02-04 15:18:28 +0800

    judge flash support

--- app/src/main/java/space/kiya/flashlightsoul/Flashlight.java
+++ app/src/main/java/space/kiya/flashlightsoul/Flashlight.java
@@ -1,8 +1,11 @@
 package space.kiya.flashlightsoul;
+import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.Toast;
 /**
  * Created by kiya on 16-2-3.
  */
@@ -19,5 +22,13 @@ public class Flashlight extends BaseActivity{
         layoutParams.width = point.x / 3;
 
         mFlashlightController.setLayoutParams(layoutParams);
+    }
+
+    public void onclick_flashlight(View view){
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+            Toast.makeText(this,"您的设备不支持闪光灯",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(this,"您的设备支持闪光灯!",Toast.LENGTH_SHORT).show();
     }
 }
